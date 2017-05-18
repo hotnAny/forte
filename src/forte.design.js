77400087d0be47a1b4f7aec37431f5d38f7fbd7e
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-05-18 15:44:19 -0400

    added a design object to hold the info

---
+++ src/forte.design.js
@@ -0,0 +1,19 @@
+// ......................................................................................................
+//
+//  a class of forte design
+//
+//  by xiangchen@acm.org, v0.0, 05/2017
+//
+// ......................................................................................................
+
+FORTE.Design = function() {
+    this.designPoints = [];
+    this.emptyPoints = [];
+    this.loadPoints = [];
+    this.loadValues = [];
+    this.boundaryPoints = [];
+};
+
+FORTE.Design.prototype = {
+    constructor: FORTE.Design
+}
