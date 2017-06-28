03b7b361d4214b1b687f0a6136c540a5ece64f17
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-06-28 16:17:47 -0400

    moved more comm related code around

--- src/forte.comm.js
+++ src/forte.comm.js
@@ -45,3 +45,63 @@ FORTE.readStressData = function () {
         );
     }
 }
+
+//
+//
+//
+FORTE.readOptimizationOutput = function () {
+    var baseDir = FORTE.outDir + '/' + FORTE.trial;
+    XAC.readTextFile(baseDir + '_' + (FORTE.itrCounter + 1) + '.out',
+        // on success
+        function (text) {
+            FORTE.fetchInterval = Math.max(FORTE.FETCHINTERVAL * 0.75, FORTE.fetchInterval * 0.9);
+            var bitmap = FORTE.getBitmap(text);
+            FORTE.design.bitmaps.push(bitmap);
+            if (FORTE.itrCounter >= FORTE.DELAYEDSTART && !FORTE.renderStarted) {
+                FORTE.renderInterval = FORTE.RENDERINTERVAL;
+                FORTE.render(0);
+                FORTE.renderStarted = true;
+                time();
+            }
+
+            time('fetched data for itr# ' + (FORTE.itrCounter + 1) +
+                ' after failing ' + FORTE.failureCounter + ' time(s)');
+            FORTE.itrCounter += 1;
+            setTimeout(FORTE.fetchData, FORTE.fetchInterval);
+            FORTE.failureCounter = 0;
+        },
+        // on failure
+        function () {
+            FORTE.__misses++;
+            FORTE.fetchInterval = Math.max(FORTE.FETCHINTERVAL * 2.5, FORTE.fetchInterval * 1.1);
+            if (FORTE.itrCounter == 0) {
+                setTimeout(FORTE.fetchData, FORTE.fetchInterval);
+                // FORTE.fetchInterval *= 1.1;
+            } else {
+                FORTE.failureCounter++;
+                if (FORTE.failureCounter > FORTE.GIVEUPTHRESHOLD) {
+                    FORTE.state = 'finished';
+                    log('data fetching finished');
+
+                    // update tags
+                    var numLayers = Object.keys(FORTE.htOptimizedLayers).length;
+                    var label = 'layer ' + (numLayers + 1);
+                    FORTE.htOptimizedLayers[label] = FORTE.optimizedLayer;
+                    var tag = FORTE.optimizedLayerList.tagit('createTag', label);
+                    FORTE.showOptimizedLayer(tag, label);
+
+                    //  read stresses
+                    FORTE.readStressData();
+
+                    log('misses: ' + FORTE.__misses);
+
+                    FORTE.resetButtonFromOptimization($('#btnGetVariation'), FORTE.LABELGETVARIATION);
+                    FORTE.resetButtonFromOptimization($('#btnAddStructs'), FORTE.LABELADDSTRUCTS);
+
+                    XAC.pingServer(FORTE.xmlhttp, 'localhost', '1234', [], []);
+                } else {
+                    setTimeout(FORTE.fetchData, FORTE.fetchInterval);
+                }
+            }
+        });
+}
